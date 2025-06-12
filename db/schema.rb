545b6222e063e641315a61e1515499cba571e525
kareena tomiah <kareena1508@gmail.com>
2025-06-12 15:06:28 +0400

    updated the style and the select option

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_12_073348) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_12_083037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -90,8 +90,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_12_073348) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "reviews", "flats"
-  add_foreign_key "reviews", "users"
   add_foreign_key "bookings", "flats"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "flats"
