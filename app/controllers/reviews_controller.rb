88e6d1669dc266a72e577987bb14c74df5f7c080
kareena tomiah <kareena1508@gmail.com>
2025-06-13 12:47:11 +0400

    added reviews and review form to show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,59 +1,50 @@
 class ReviewsController < ApplicationController
-  before_action :set_flat, only: [:edit, :update, :destroy]
-  before_action :set_review, only: [:edit, :update, :destroy, :show]
+  before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :set_flat, only: [:create, :edit, :update, :destroy]
 
   def create
-    @flat = Flat.find(review_params[:flat_id])
-    @review = Review.new(review_params)
+    @review = @flat.reviews.build(review_params)
     @review.user = current_user
-    @review.flat = @flat
-      if @review.save
-        redirect_to pages_experiences_path, notice: "Review posted!"
-      else
-        @reviews = Review.includes(:user, :flat).order(created_at: :desc)
-        render "pages/experiences", status: :unprocessable_entity
-      end
+
+    if @review.save
+      redirect_to flat_path(@flat), notice: "Avis ajouté avec succès."
+    else
+      @reviews = @flat.reviews.order(created_at: :desc)
+      @booking = Booking.new
+      render "flats/show", status: :unprocessable_entity
+    end
   end
-
-  def show
-    @review = Review.find(params[:id])
-  end
-
-
 
   def edit; end
 
   def update
     if @review.update(review_params)
-      redirect_to pages_experiences_path, notice: "Review updated!"
+      redirect_to flat_path(@flat), notice: "Avis modifié avec succès."
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-
   def destroy
     @review.destroy
-    redirect_to pages_experiences_path, notice: "Review deleted!"
-  end
-
-  def index
-    @reviews = Review.includes(:user, :flat).order(created_at: :desc)
+    redirect_to flat_path(@flat), notice: "Avis supprimé."
   end
 
   private
 
   def set_flat
-    @review = Review.find(params[:id])
-    @flat = @review.flat
+    if params[:flat_id]
+      @flat = Flat.find(params[:flat_id])
+    else
+      @flat = @review.flat
+    end
   end
-
 
   def set_review
     @review = Review.find(params[:id])
   end
 
   def review_params
-    params.require(:review).permit(:content, :rating, :flat_id)
+    params.require(:review).permit(:content, :rating, :title)
   end
 end
